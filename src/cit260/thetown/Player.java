8ea65adc4450cc40c018fa87ed791128445a618e
whitty66 <ericwaynewhitlock@gmail.com>
2015-05-12 11:54:16 -0600

    5/12/15 - Initial commit to repository

---
+++ src/cit260/thetown/Player.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cit260.thetown;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ *
+ * @author Eric
+ */
+public class Player implements Serializable{
+    
+    //class instance variable
+    private String name;
+
+    public Player() {
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" + "name=" + name + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 89 * hash + Objects.hashCode(this.name);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Player other = (Player) obj;
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+}
