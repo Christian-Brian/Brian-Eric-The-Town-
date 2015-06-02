84b2ac96cf65fcb69ab23d0d1ff56b27b74681e6
whitty66 <ericwaynewhitlock@gmail.com>
2015-06-02 12:36:47 -0600

    5/29/2015 - doing view stuff

---
+++ src/citbyui/cit260/thetown/view/StartProgramView.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package citbyui.cit260.thetown.view;
+
+/**
+ *
+ * @author Eric
+ */
+public class StartProgramView {
+    
+    public StartProgramView() {
+        
+    }
+    
+    public void startProgram() {
+        // Display the banner screen
+        this.displayBanner();
+        
+        //prompt the player to enter their name
+        String playersName = this.getPlayersName();
+      
+}
+
+    private void displayBanner() {
+        
+        System.out.println("\n\n***************************************");
+        
+        System.out.println("*                                          *"
+                        + "\n*           Welcome to The Town.           "
+                        + "\n*       A text based adventure game.       ");
+        
+        System.out.println("*                                         *"
+                        + "\n* Temporarily marooned in a strange town, *"
+                        + "\n* you must explore the surrounding areas  *"
+                        + "\n* North, South, East, and West of town    *"
+                        + "\n* searching for resources and information *"
+                        + "\n* to help you leave town unharmed.        *"
+                        + "\n* Each time you play you have 1 hour      *"
+                        + "\n* to complete the game.                   *");
+        
+        System.out.println("*                                          *"
+                        + "\n*          Have fun adventuring!          *"
+                        + "\n*                                         *");
+        
+        System.out.println("\n\n****************************************");
+    }
+
+    private String getPlayersName() {
+        boolean valid = false; //indicates if the name has been retrived
+                String playersName = null;
+                Scanner keyboard = new Scanner(system.in); //keyboard input stream
+                
+                while(!valid) { //while a valid name has not been retrived
+                    
+                        //prompt for the players name
+                        System.out.println("Enter the characters name below:");
+                        
+                        //get the name from the keyboard and trim off the blanks
+                        playersName = keyboard.nextLine();
+                        playersName = keyboard.trim();
+                        
+                        //if the name is invalid
+                        if (playersName.length() < 2) {
+                            System.out.println("Invalid name - the name must not be blank");
+                            continue; //and repeat again
+                        }
+                        break; //out of the repitition (exit)
+                    }
+                
+    }
+    return playersName; //return the name
+    }
--- src/the/town/TheTown.java
+++ src/the/town/TheTown.java
@@ -15,6 +15,7 @@ import citbyui.cit260.thetown.model.Resources;
 import citbyui.cit260.thetown.model.Game;
 import citbyui.cit260.thetown.model.Locations;
 import citbyui.cit260.thetown.model.Map;
+import citbyui.cit260.thetown.view.StartProgramView;
 
 /**
  *
@@ -26,85 +27,89 @@ public class TheTown {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Player playerOne = new Player();
         
-        playerOne.setName("Jordan Taylor");
+        StartProgramView startProgramView = new StartProgramView();
+        startProgramView.startProgram();
         
-        String playerInfo = playerOne.toString();
-        System.out.println(playerInfo);
-        
-        Characters characterOne = new Characters();
-        Characters description = new Characters();
-        
-        characterOne.setName("Mary");
-        description.setDescription("someone with info in town");
-        
-        String characterInfo = characterOne.toString();
-        String descriptionInfo = description.toString();
-        System.out.println(characterOne);
-        System.out.println(description);
-        
-        Game gameStartTime = new Game();
-        
-        gameStartTime.setStartTime(1200);
-        
-        String startTimeInfo = gameStartTime.toString();
-        System.out.println(startTimeInfo);
-        
-        Locations ammount = new Locations();
-        Locations restrictedAccess = new Locations();
-        Locations coordinants = new Locations();
-        
-        ammount.setAmount(0/1);
-        restrictedAccess.setRestrictedAccess("Your Path Is Blocked.");
-        coordinants.setCoordinants("A-6");
-                
-        String amountInfo = ammount.toString();
-        String restrictedAccessInfo = restrictedAccess.toString();
-        String coordinantsInfo = coordinants.toString();
-        System.out.println(amountInfo);
-        System.out.println(restrictedAccessInfo);
-        System.out.println(coordinantsInfo);
-        
-        Map numberOfColumns = new Map();
-        Map numberOfRows = new Map();
-        
-        numberOfColumns.setNumberOfColumns(25);
-        numberOfRows.setNumberOfRows(25);
-        
-        String numberOfColumnsInfo = numberOfColumns.toString();
-        String numberOfRowsInfo = numberOfRows.toString();
-        System.out.println(numberOfColumnsInfo);
-        System.out.println(numberOfRowsInfo);
-        
-        // brian
-        Scene mountain = new Scene();
-        mountain.setDescription("mountains far away");
-        mountain.setResourceType("resources to look for");
-     
-        String mountainInfo = mountain.toString();
-        System.out.println(mountainInfo);
-        
-        Worlds nameOne = new Worlds();
-        nameOne.setName("the name");
-        nameOne.setDescription("the description for worlds");
-        
-        String nameOneInfo = nameOne.toString();
-        System.out.println(nameOneInfo);
-        
-        Messages hint = new Messages();
-        hint.setHint("your hint");
-        
-        String hintInfo = hint.toString();
-        System.out.println(hintInfo);
-        
-        Resources res = new Resources();
-        res.setName("the name");
-        res.setType("the type");
-        res.setAmountOfGold(50.254);
-        
-        String resInfo = res.toString();
-        System.out.println(resInfo);
+//        Player playerOne = new Player();
+//        
+//        playerOne.setName("Jordan Taylor");
+//        
+//        String playerInfo = playerOne.toString();
+//        System.out.println(playerInfo);
+//        
+//        Characters characterOne = new Characters();
+//        Characters description = new Characters();
+//        
+//        characterOne.setName("Mary");
+//        description.setDescription("someone with info in town");
+//        
+//        String characterInfo = characterOne.toString();
+//        String descriptionInfo = description.toString();
+//        System.out.println(characterOne);
+//        System.out.println(description);
+//        
+//        Game gameStartTime = new Game();
+//        
+//        gameStartTime.setStartTime(1200);
+//        
+//        String startTimeInfo = gameStartTime.toString();
+//        System.out.println(startTimeInfo);
+//        
+//        Locations ammount = new Locations();
+//        Locations restrictedAccess = new Locations();
+//        Locations coordinants = new Locations();
+//        
+//        ammount.setAmount(0/1);
+//        restrictedAccess.setRestrictedAccess("Your Path Is Blocked.");
+//        coordinants.setCoordinants("A-6");
+//                
+//        String amountInfo = ammount.toString();
+//        String restrictedAccessInfo = restrictedAccess.toString();
+//        String coordinantsInfo = coordinants.toString();
+//        System.out.println(amountInfo);
+//        System.out.println(restrictedAccessInfo);
+//        System.out.println(coordinantsInfo);
+//        
+//        Map numberOfColumns = new Map();
+//        Map numberOfRows = new Map();
+//        
+//        numberOfColumns.setNumberOfColumns(25);
+//        numberOfRows.setNumberOfRows(25);
+//        
+//        String numberOfColumnsInfo = numberOfColumns.toString();
+//        String numberOfRowsInfo = numberOfRows.toString();
+//        System.out.println(numberOfColumnsInfo);
+//        System.out.println(numberOfRowsInfo);
+//        
+//        // brian
+//        Scene mountain = new Scene();
+//        mountain.setDescription("mountains far away");
+//        mountain.setResourceType("resources to look for");
+//     
+//        String mountainInfo = mountain.toString();
+//        System.out.println(mountainInfo);
+//        
+//        Worlds nameOne = new Worlds();
+//        nameOne.setName("the name");
+//        nameOne.setDescription("the description for worlds");
+//        
+//        String nameOneInfo = nameOne.toString();
+//        System.out.println(nameOneInfo);
+//        
+//        Messages hint = new Messages();
+//        hint.setHint("your hint");
+//        
+//        String hintInfo = hint.toString();
+//        System.out.println(hintInfo);
+//        
+//        Resources res = new Resources();
+//        res.setName("the name");
+//        res.setType("the type");
+//        res.setAmountOfGold(50.254);
+//        
+//        String resInfo = res.toString();
+//        System.out.println(resInfo);
      
     }
     
