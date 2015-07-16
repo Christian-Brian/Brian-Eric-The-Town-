113d38038ac10bd21a37c0fb5250fa4e18c32a96
whitty66 <ericwaynewhitlock@gmail.com>
2015-07-16 17:02:12 -0600

    Week 12 final changes

--- src/citbyui/cit260/thetown/model/Characters.java
+++ src/citbyui/cit260/thetown/model/Characters.java
@@ -16,32 +16,32 @@ public enum Characters implements Serializable {
 
     //enum attributes
 
-    Mark("I heard you were looking to find "
-            + "\ntreasure in these mountains. I've searched for years and haven't"
-            + "\ngot any richer. Take my shovel with you if you're gonna go look."
-            + "\nAdam the goldsmith walks out there every day. Hind him if you need help"),
-    Adam("I found treasure. Finally! Help me open it and you can have half."),
-    John("If you think you're tough, try your hand east of town. It's a good "
-            + "\nway to make money if you're good. Talk to Wayne about competing "
-            + "\nout there."),
-    Wayne("Out east is the most fierce rock, paper, scissors style fighting in "
+    Mark("Marks Message [9,6]: I heard you were looking to find treasure in these "
+            + "\nmountains. I've searched for years and haven't got any richer."
+            + "\nTake my shovel with you if you're gonna go look.Adam the "
+            + "\ngoldsmith walks out there every day. Hind him if you need help"),
+    Adam("Adams Message [8,2]: I found treasure. Finally! Help me open it and you can have half."),
+    John("Johns Message [6,7]: If you think you're tough, try your hand east of town."
+            + "\nIt's a good way to make money if you're good. Talk to Wayne about"
+            + "\ncompeting out there."),
+    Wayne("Waynes Message [5,8]: Out east is the most fierce rock, paper, scissors style fighting in "
             + "\nthe world. Take this badge. You'll need it to fight. Look around"
             + "\nfor a bandit and get ready to get beat."),
-    Rachael("Not to far south of here is a pretty incredible beach. I go and "
+    Rachael("Rachels Message[7,8]: Not to far south of here is a pretty incredible beach. I go and "
             + "\ncollect shells and dive for pearls and make jewelery with them."
             + "\nMy friend Selina is out there right now."),
-    Selina("I've had some good luck today finding rare shells. I keep all the "
+    Selina("Selinas Mssage [6,12]: I've had some good luck today finding rare shells. I keep all the "
             + "\noyster pearls I find in this water tank I made. "
             + "\nIf you answer this problem i'll share some with you."),
-    Abraham("No one knows whats out there. Most people who explore west don't "
+    Abraham("Abrahams Message [6,6]: No one knows whats out there. Most people who explore west don't "
             + "\ncome back. They say they found whats beyond the mist. I've "
             + "\ntraveled west farther than you can travel in the desert. "
             + "\nI've been trying my whole life to clear the mist. Alfred and I "
             + "\nare going to take another trip. Maybe for good."),
-    Alfred("Once we step into the mist i have no plans of "
+    Alfred("Alfreds Message [9,9]: Once we step into the mist i have no plans of "
             + "\ncomming back to this town. We'll either break the mist or die out"
             + "\nthere. Take this extra cloak if you decide to adventure west.)"),
-    Gordon("There's not enough women in this town. Thats why "
+    Gordon("Gordons Message [8,5]: There's not enough women in this town. Thats why "
             + "\nAbe and Alfred are leaving. To find more women. Whatever. Evens "
             + "\nout the ratio here.");
 
